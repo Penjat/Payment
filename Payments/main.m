95e2f748c1275b245c8a5fd3298d7f0a50bf5d26
Spencer Symington <spencersymington@Spencers-MacBook-Pro.local>
2019-01-10 20:20:36 -0800

    basic setup

--- Payments/main.m
+++ Payments/main.m
@@ -10,6 +10,17 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
+        int toPay = arc4random_uniform(900)+100;
+        
+        NSLog(@"Thank you for shopping at Acme.com Your total today is $xxx Please select your payment method: 1: Paypal, 2: Stripe, 3: Amazon");
+        
+        char answerInput[50];
+        fgets(answerInput,50,stdin);
+        
+        NSString * userInput = [NSString stringWithCString:answerInput encoding:NSUTF8StringEncoding];
+        int userSelection = [userInput integerValue];
+        
+        NSLog(@"the user selected: %i",userSelection);
         
     }
     return 0;
