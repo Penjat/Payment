70c627609afb9765853a445810992765b7b5b4fd
Spencer Symington <spencersymington@spencers-macbook-pro.local>
2019-01-10 20:13:17 -0800

    Initial Commit

---
+++ Payments/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Payments
+//
+//  Created by Spencer Symington on 2019-01-10.
+//  Copyright © 2019 Spencer Symington. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
